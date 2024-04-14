4e93a6c547cd29f331cdf461a593dc8a706dea82
Anthony-Kuismi <kuism006@d.umn.edu>
2024-04-14 10:48:43 -0500

    improved UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,16 @@ export default function Navbar(){
             <nav className='nav'>
                 <h1 className='site-title'>Anthony Kuismi</h1>
                 <ul>
+                    <ion-icon name='home'></ion-icon>
                     <NavbarLink to='/developer-portfolio/'>Home</NavbarLink>
+                    <ion-icon name='folder'></ion-icon>
                     <NavbarLink to='/developer-portfolio/projects/'>Projects</NavbarLink>
+                    <ion-icon name='information-circle'></ion-icon>
                     <NavbarLink to='/developer-portfolio/about-me/'>About Me</NavbarLink>
+                    <ion-icon name='logo-linkedin'></ion-icon>
+                    <a href='https://www.linkedin.com/in/anthony-kuismi' target='_blank'>LinkedIn</a>
+                    <ion-icon name='logo-github'></ion-icon>
+                    <a href='https://github.com/Anthony-Kuismi' target='_blank'>GitHub</a>
                 </ul>
             </nav>
             <hr/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,16 @@ import './Home.css'
 export default function Home(){
     return(
         <div className='home-container'>
-            <div className='quote'>
-            “Intellectual growth should commence at birth and cease only at death.” - Albert Einstein
+            <div className='main-container'>
+                <div className='title'>
+                    Howdy, I'm Anthony Kuismi!
+                </div>
+                <div className='subtitle'>
+                    Computer Science BS & Mathematics Minor @University of Minnesota Duluth
+                </div>
+                <div className='message'>
+                    Always eager to learn.
+                </div>
             </div>
         </div>
     );
