ef59a0cbb469033dc9fcb773af7c1ec01a3c1156
Anthony-Kuismi <kuism006@d.umn.edu>
2024-04-15 16:35:56 -0500

    added randomize and populate buttons on sorting page

--- src/components/VisualArray.jsx
+++ src/components/VisualArray.jsx
@@ -1,20 +1,71 @@
-import { useRef } from "react";
+import { useEffect,useCallback, useRef, useState } from "react";
 import './VisualArray.css'
-export default function VisualArray(){
+function VisualArray() {
     const divRef = useRef(null);
-    const count = 100;
-    const height = 20; 
-    var array = [];
-    for(let i=1; i <= count; i++){
-        array.push(i/count * height);
+    const height = 20;
+    var array = useRef([]);
+    var width = useRef(0);
+    const [, updateState] = useState();
+    const forceUpdate = useCallback(() => updateState({}), []);
+
+    // Fisher–Yates (aka Knuth) Shuffle.
+    function shuffleArray() {
+        let currentIndex = array.current.length;
+
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array.current[currentIndex], array.current[randomIndex]] = [
+                array.current[randomIndex], array.current[currentIndex]];
+        }
+        forceUpdate();
+        console.log(array.current);
     }
-    var width = 100/array.length;
-    
-    return(
-        <div className='array-container'ref={divRef}>
-            {array.map((item) =>{
-                return <div key={item}style={{width:`${width}%`,height: `${item}rem`}}></div>
-            })}
+
+    function populateArray(){
+        let count = 10;
+        for (let i = 1; i <= count; i++) {
+            array.current.push(i / count * height);
+        }
+        width.current = 100 / count;
+        forceUpdate();
+    }
+
+    useEffect(() => {
+        const clearDisplay = () => {
+            while (divRef.current.lastChild != null) {
+                divRef.current.removeChild(divRef.current.lastChild);
+            }
+        }
+
+        const addDiv = (height) => {
+            const div = document.createElement('div');
+            div.style.height = `${height}rem`;
+            div.style.width = `${width.current}%`;
+            divRef.current.appendChild(div);
+        }
+
+        const showArray = () => {
+            console.log(array.current);
+            clearDisplay();
+            array.current.forEach(item => {
+                addDiv(item);
+            });
+        }
+
+        showArray();
+    });
+
+    return (
+        <div className='sorting-container'>
+            <div className='array-container' ref={divRef}>
+            </div>
+            <div className='button-container'>
+                <button type='button' onClick={shuffleArray}>Randomize</button>
+                <button type='button' onClick={populateArray}>Populate</button>
+            </div>
         </div>
     );
 }
+
+export default VisualArray;
